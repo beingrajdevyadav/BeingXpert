7b4d799c2985dcc5e9161930d83463eb3007fc5d
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-31 02:24:24 +0530

    Offer Letter Component Added.

---
+++ pages/OfferLetter.js
@@ -0,0 +1,12 @@
+const OfferLetter = () => {
+  return (
+    <div>
+      <h1>Offer Letter</h1>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzspy0YHEg3C1i3fGtQM6sBHpo4oLdDttPGQFW9JFN6wQP1czup34UO_5yBl4Vn59s-Yk&usqp=CAU" alt="working" />
+        <h2>Working On...</h2>
+
+    </div>
+  );
+};
+
+export default OfferLetter;
