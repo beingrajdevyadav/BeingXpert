ba3fba8a41c5a7f2c9368317054f8eb341d5375f
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-31 02:31:37 +0530

    Report Component Added.

---
+++ pages/Reports.js
@@ -0,0 +1,19 @@
+const Reports = ()=>{
+    return (
+        <div>
+            <h1>Reports</h1>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzspy0YHEg3C1i3fGtQM6sBHpo4oLdDttPGQFW9JFN6wQP1czup34UO_5yBl4Vn59s-Yk&usqp=CAU" alt="working"/>
+            <p>Here are the reports we offer</p>
+            <h2>Available Reports</h2>
+            <ul>
+                <li>Report 1</li>
+                <li>Report 2</li>
+                <li>Report 3</li>
+                <li>Report 4</li>
+            </ul>
+            <h2>Working On...</h2>
+        </div>
+    )
+};
+
+export default Reports;
