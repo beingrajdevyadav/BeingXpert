29dcdbcc7430a99976d614c026e78a4784693ab9
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-31 00:21:23 +0530

    Courses Component Added.

---
+++ pages/Courses.js
@@ -0,0 +1,16 @@
+const Courses = ()=>{
+    return(
+        <div>
+            <h1>Courses</h1>
+            <p>Here are the courses we offer</p>
+            <ul>
+                <li>Course 1</li>
+                <li>Course 2</li>
+                <li>Course 3</li>
+                <li>Course 4</li>
+            </ul>
+
+            <h2>Working On...</h2>
+        </div>
+    )
+}
