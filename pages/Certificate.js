25e2aeb449d4d0825d2300a596f26ee081fc6a8c
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-31 00:18:57 +0530

    Certificate Component Added.

---
+++ pages/Certificate.js
@@ -0,0 +1,13 @@
+const Certificate = ()=>{
+    return(
+        <div>
+            <h1>Certificate</h1>
+            <img src="https://cdn.dribbble.com/users/7421625/screenshots/19193172/media/02fae889dbcce29d1519c8523a859be8.gif" alt="working"/>
+            <h2>Welcome To Certificate Page </h2>
+            <h1>Working On..</h1>
+
+        </div>
+    )
+};
+
+export default Certificate;
