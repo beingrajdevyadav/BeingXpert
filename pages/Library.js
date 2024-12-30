27061797c1a1e2a9c9d4c3908ddf4dcf65233b9d
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-31 00:47:02 +0530

    Library Component Added.

---
+++ pages/Library.js
@@ -0,0 +1,19 @@
+const Library = () => {
+    return (
+        <div>
+            <h1>Welcome to Library</h1>
+            <img src="https://cdn.dribbble.com/users/3951514/screenshots/7172485/librarian_gif_edited.gif" alt="working" />
+
+            <ul>
+                <li>Book 1</li>
+                <li>Book 2</li>
+                <li>Book 3</li>
+                <li>Book 4</li>
+                <li>Book 5</li>
+            </ul>
+            <h2>Working On..</h2>
+        </div>
+    )
+};
+
+export default Library;
