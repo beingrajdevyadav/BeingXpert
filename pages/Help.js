216b9113491a2ae70df0137ac681ee8c24ebb8f6
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-31 00:33:02 +0530

    Help Component Added.

--- pages/Help.js
+++ pages/Help.js
@@ -2,7 +2,10 @@ const Help = ()=>{
     return(
         <div>
             <h1>Help</h1>
+            <img src="https://cdn.dribbble.com/users/132658/screenshots/14228126/team.gif" alt="working" />
+            <h2>Working On...</h2>
             <p>Help page content</p>
         </div>
     )
-}
+};
+export default Help;
