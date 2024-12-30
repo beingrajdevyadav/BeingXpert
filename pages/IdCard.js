cc1a4923775ff996cdc80ead1443d0f9bbda23b7
Rajdev Yadav <myself.rajdev2024@gmail.com>
2024-12-31 00:41:53 +0530

    Id Card Component added.

---
+++ pages/IdCard.js
@@ -0,0 +1,13 @@
+const IdCard = ()=>{
+
+    return(
+        <div>
+            <h1>IdCard</h1>
+            <img src="https://cdn.dribbble.com/users/368135/screenshots/5713705/kyc-verifying-circle.gif" alt="working"/>
+
+            <h2> Working On...</h2>
+        </div>
+    )
+};
+
+export default IdCard;
