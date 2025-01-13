cabe1463ff555efe35205f432fd3d0a96309ff16
Rajdev Yadav <myself.rajdev2024@gmail.com>
2025-01-13 23:44:07 +0530

    downloadPDF features added in students details page.

--- JS/StudentsFunctionality.js
+++ JS/StudentsFunctionality.js
@@ -1,29 +1,49 @@
+
 import { students } from '../sample_data.js'
-        // console.log(students);
 
-        // function to print students table
-        function PrintStudentsTable(students) {
-            const tableBody = document.querySelector("#StudentsTableBody");
 
-            let tbody = '';
+// function to print students table
+function PrintStudentsTable(students) {
+    const tableBody = document.querySelector("#StudentsTableBody");
 
-            students.forEach((student, i) => {
-                tbody += `
-                       <tr>
-                       <td>${i + 1}</td> 
-                       <td>${student.studentId}</td> 
-                       <td>${student.firstName} ${student.lastName}</td> 
-                       <td>${student.major}</td> 
-                       <td>${student.gpa}</td> 
-                       <td>${student.idCardStatus}</td> 
-                       <td>${student.libraryStatus}</td> 
-                       <td>${student.enrollmentDate}</td> 
-                    </tr>
+    let tbody = '';
+
+    students.forEach((student, i) => {
+        tbody += `
+               <tr>
+               <td>${i + 1}</td> 
+               <td>${student.studentId}</td> 
+               <td>${student.firstName} ${student.lastName}</td> 
+               <td>${student.major}</td> 
+               <td>${student.gpa}</td> 
+               <td>${student.idCardStatus}</td> 
+               <td>${student.libraryStatus}</td> 
+               <td>${student.enrollmentDate}</td> 
+            </tr>
 `;
-            });
+    });
 
-            tableBody.innerHTML = tbody;
-        };
+    tableBody.innerHTML = tbody;
+};
 
-        PrintStudentsTable(students);
-        console.log(students);
+PrintStudentsTable(students);
+console.log(students);
+
+// functions to download pdf
+// download btn selection and then adding event listener on click
+document.getElementById("downloadPDF").addEventListener("click", function () {
+
+    // selection of table 
+    const table = document.getElementById("studentsDetailsTable");
+
+    // converting html into canvas -->> scale value define pdf high quality 
+    html2canvas(table, { scale: 4 }).then(function (canvas) {
+
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jspdf.jsPDF("landscape", "px", [canvas.width, canvas.height]);
+
+        pdf.addImage(imgData, "PNG", 0, 0, canvas.width, canvas.height);
+        // to save pdf
+        pdf.save("StudentsDetailsTable.pdf");
+    });
+});
